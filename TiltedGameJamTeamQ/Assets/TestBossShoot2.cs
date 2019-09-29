220652a2e010bab4cf7d6726f0e2025b3806d6ab
michacg <michacg2@uci.edu>
2019-09-28 19:00:41 -0700

    Phase three and four done

--- TiltedGameJamTeamQ/Assets/TestBossShoot2.cs
+++ TiltedGameJamTeamQ/Assets/TestBossShoot2.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestBossShoot : MonoBehaviour
+public class TestBossShoot2 : MonoBehaviour
 {
     [SerializeField] GameObject bullet;
     [SerializeField] float angleInBetween;
@@ -42,41 +42,29 @@ public class TestBossShoot : MonoBehaviour
 
     IEnumerator SpawnBullets()
     {
-        float revs = 0f;
         angle = 0f;
         List<GameObject> movement = new List<GameObject>();
         isShooting = true;
         float radius = pool.GetRadius();
         float angleInBetween = pool.getAngleInBetween();
         float spawnLag = pool.getSpawnLag();
-
-        while (revs < 3f)
+        while (angle < 360f)
         {
-            while (angle < 360f)
-            {
-                float rad = Mathf.Deg2Rad * angle;
-                GameObject b = pool.GetUnusedObject();
-                b.SetActive(true);
-                b.GetComponent<SpriteRenderer>().enabled = false;
-                b.transform.localPosition = Vector3.Normalize(new Vector3(Mathf.Cos(rad), Mathf.Sin(rad))) * (radius);
-                movement.Add(b);
-                yield return new WaitForSeconds(spawnLag);
-                angle += angleInBetween;
-
-            }
-            foreach (GameObject b in movement)
-            {
-                b.GetComponent<SpriteRenderer>().enabled = true;
-                b.GetComponent<BulletMovement>().FireOff(pool.transform.localPosition, pool.GetBulletSpeed());
-            }
-
-            Debug.Log(radius + (revs * 0.2f));
-            revs += 1f;
-            angle = 0f;
-            movement = new List<GameObject>();
-            
+            float rad = Mathf.Deg2Rad * angle;
+            GameObject b = pool.GetUnusedObject();
+            b.SetActive(true);
+            b.GetComponent<SpriteRenderer>().enabled = false;
+            b.transform.localPosition = Vector3.Normalize(new Vector3(Mathf.Cos(rad), Mathf.Sin(rad))) * Random.Range(radius, radius + 0.1f);
+            movement.Add(b);
+            yield return new WaitForSeconds(spawnLag);
+            angle += angleInBetween + Random.Range(-10f, 10f);
         }
+        foreach (GameObject b in movement)
+        {
+            b.GetComponent<SpriteRenderer>().enabled = true;
 
+            b.GetComponent<BulletMovement>().FireOff(pool.transform.localPosition, pool.GetBulletSpeed());
+        }
         isShooting = false;
         waitingToShoot = false;
     }
